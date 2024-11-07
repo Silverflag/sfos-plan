d3a79c19acd20294f8e125a0404aacf13092af19
NotHacking (https://silverflag.net) <yetanotherdingus@gmail.com>
2024-11-07 01:23:59 +0100

    sfositectures

--- branding.sh
+++ branding.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Path to the directory containing the uncompiled OS
-sfos_dir="/path/to/sfos"
-# Better logic 
-find "$sfos_dir" -type f -exec sed -i 's/Arch Linux/sfOS/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/arch linux/sfos/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/ARCH LINUX/SFOS/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/archlinux/sfos/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/Archlinux/sfos/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/archLinux/sfos/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/ARCHLINUX/SFOS/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/Arch/sfos/g' {} +
-find "$sfos_dir" -type f -exec sed -i 's/arch/sfos/g' {} +
+sfos_dir="sfos"
+# fix sfositectures
+find "$sfos_dir" -type f -exec sed -i 's/\bArch Linux\b/sfOS/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\barch linux\b/sfos/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\bARCH LINUX\b/SFOS/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\barchlinux\b/sfos/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\bArchlinux\b/sfos/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\barchLinux\b/sfos/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\bARCHLINUX\b/SFOS/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\bArch\b/sfos/g' {} +
+find "$sfos_dir" -type f -exec sed -i 's/\barch\b/sfos/g' {} +
 echo "Branding updated directly in the 'sfos' directory."
